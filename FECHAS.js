0386915f4c4bdd60a5b0229c9b1846d60a3f9c2b
Marin37 <57327931+Marin37@users.noreply.github.com>
2020-11-12 16:05:07 -0300

    fechas.js

---
+++ FECHAS.js
@@ -0,0 +1,14 @@
+let mes;
+let dia;
+let año;
+
+function randomf() {
+dia = Math.floor(Math.random() * (30 - 1 + 1)) + 1;
+mes = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+año = Math.floor(Math.random() * (2001 - 1976 + 1)) + 1976;
+return(año + "/" + mes + "/" + dia);
+};
+
+for(var i = 0; i < 140; i++) {
+    randomf();
+}
